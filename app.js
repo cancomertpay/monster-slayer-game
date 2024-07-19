ebea8ef5c6d3890daef2ec9e9e705127b5cebfdf
Can Cömertpay <cancomertpay@gmail.com>
2024-07-19 14:48:42 +0300

    Document ready to deploy

---
+++ app.js
@@ -0,0 +1,104 @@
+// UTILS
+const getRandomValue = (min, max) =>
+  Math.floor(Math.random() * (max - min) + min);
+
+// VUE
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      currentRound: 0,
+      winner: null,
+      logMessages: [],
+    };
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        // A draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // Player lost
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        // A draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // Monster lost
+        this.winner = "player";
+      }
+    },
+  },
+  computed: {
+    monsterBarStyles() {
+      if (this.monsterHealth <= 0) {
+        return { width: "0%" };
+      }
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      if (this.playerHealth <= 0) {
+        return { width: "0%" };
+      }
+      return { width: this.playerHealth + "%" };
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
+  },
+  methods: {
+    startNewGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+      this.logMessages = [];
+    },
+    attackMonster() {
+      this.currentRound++;
+      const damageValue = getRandomValue(5, 12);
+      this.monsterHealth -= damageValue;
+      this.addLogMessage("player", "attack", damageValue);
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const damageValue = getRandomValue(8, 15);
+      this.playerHealth -= damageValue;
+      this.addLogMessage("monster", "attack", damageValue);
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.addLogMessage("player", "special-attack", attackValue);
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+      this.addLogMessage("player", "heal", healValue);
+    },
+    surrender() {
+      this.winner = "monster";
+      this.addLogMessage("player", "surrender", 0);
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
+    },
+  },
+});
+
+app.mount("#game");
